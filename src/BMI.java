cb911f8883ba6fb16139ee61d921f6567894ef30
nok-g <znihei612@gmail.com>
2023-11-26 18:31:11 +0900

    BMI branches in progress

--- src/BMI.java
+++ src/BMI.java
@@ -5,13 +5,26 @@ public class BMI {
     public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your height");
+        System.out.println("Please enter your height in meter:");
         float height =  scanner.nextFloat();
-        System.out.println("Please enter your weight");
+        System.out.println("Please enter your weight: ");
         float weight =  scanner.nextFloat();
 
-        float bmi = height * weight;
+        float bmi = weight  / height / height  ;
         System.out.println("Your BMI is:" + bmi);
+
+        if  ( bmi < 16 ) {
+            System.out.println("Severe thinness");
+        }
+        else if ( bmi < 17 ) {
+            System.out.println ( " Mederate thinness " ) ;
+        }
+        else if ( bmi < 18.5 ){
+            System.out.println(" Mild thinness ") ;
+        }
+        else {
+            System.out.println(" later ");
+        }
 
     }
 }
